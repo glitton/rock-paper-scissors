807beb5854821f2ef16f5ef6d42ee13369ce34f0
Generosa Litton <glcodeworks@gmail.com>
2018-08-26 12:33:00 -0700

    first commit to rock paper scissors game

---
+++ project-1-rock-paper-scissors-x99/js/game-logic.js
@@ -0,0 +1,37 @@
+//Logic for Rock paper scissors game as seen here
+//https://s3.amazonaws.com/codecademy-content/programs/build-apis/projects/build-apis-project-1-rock-paper-scissors-x99/project/index.html
+
+// Twelve global variables representing each player's move types and values
+let playerOneMoveOneType;
+let playerOneMoveOneValue;
+let playerOneMoveTwoType;
+let playerOneMoveTwoValue;
+let playerOneMoveThreeType;
+let playerOneMoveThreeValue;
+let playerTwoMoveOneType;
+let playerTwoMoveOneValue;
+let playerTwoMoveTwoType;
+let playerTwoMoveTwoValue;
+let playerTwoMoveThreeType;
+let playerTwoMoveThreeValue;
+
+const setPlayerMoves = (
+  player,
+  moveOneType,
+  moveOneValue,
+  moveTwoType,
+  moveTwoValue,
+  moveThreeType,
+  moveThreeValue
+) => {
+  if(player === 'Player One') {
+    playerOneMoveOneType = moveOneType
+    playerOneMoveOneValue
+  }
+};
+
+const getRoundWinner = roundNumber => {
+  //compare playerOneMove type and value with playerTwo's
+}
+
+const getGameWinner;
